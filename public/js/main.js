3757fae633d6ad2306160ff269876b192efa2edb
Angelina Stone <Angelina.Stone@dxdiag.co.za>
2022-08-24 01:21:17 +0200

    using the btn event added the dim label func

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,31 @@
 const blackPianoKeys = document.querySelectorAll('.bk');
 const whitePianoKeys = document.querySelectorAll('.wk');
 
-function playSound(Keyname,clr) {
+function playSound(pianoKey,clr) {
+
+    let Keyname = pianoKey.getAttribute('value')
+    if(clr == 'white'){dimKey(pianoKey);}
     console.log(`Keyname is ${Keyname}`)
     filename = `../sound/${clr}keys/key${Keyname}.mp3`
-
-
     new Audio(filename).play();
+    
+}
+
+function dimKey(pianoKey){
+    const dimKey = pianoKey.querySelector('.Dim-key');
+    dimKey.style.display = 'block'
+    setTimeout(() => {
+        dimKey.style.display = 'none'
+    }, 300);
 }
 
 
+
+
 blackPianoKeys.forEach(pianoKey =>{
-    pianoKey.addEventListener('click', playSound.bind(null,pianoKey.getAttribute('value'),'black'));
+    pianoKey.addEventListener('click', playSound.bind(null,pianoKey,'black'));
 })
 
 whitePianoKeys.forEach(pianoKey =>{
-    pianoKey.addEventListener('click', playSound.bind(null,pianoKey.getAttribute('value'),'white'));
+    pianoKey.addEventListener('click', playSound.bind(null,pianoKey,'white'));
 })
