755ea94e24e26306032b902c37fcdd362dae590c
Angelina Stone <Angelina.Stone@dxdiag.co.za>
2022-08-23 19:34:42 +0200

    removed piano keys zip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
 let port = process.env.PORT || 1111
+global.env = process.env.NODE_ENV || 'Development'
 
-
+app.use(express.static(__dirname + '/Public')); //__dir and not _dir
+app.listen(port);
