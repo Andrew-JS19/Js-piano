696c0c3b6d5d22b4364030310f3296c9e00f14d5
Angelina Stone <Angelina.Stone@dxdiag.co.za>
2022-08-23 21:18:39 +0200

    corrected the public reference to lowercase

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 let port = process.env.PORT || 1111
 global.env = process.env.NODE_ENV || 'Development'
 
-app.use(express.static(__dirname + '/Public')); //__dir and not _dir
+app.use(express.static(__dirname + '/public')); //__dir and not _dir
 app.listen(port);
